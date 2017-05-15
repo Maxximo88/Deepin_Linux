d42e30cdd1c6d6577f45b8e2f829b1e986804da2
Massimo A. Carofano <massimo.carofano@gmail.com>
2017-05-15 09:49:29 +0200

    Update Deepin post installazione.sh

--- Deepin post installazione.sh
+++ Deepin post installazione.sh
@@ -165,7 +165,7 @@ EOF'
 			echo ''
 			echo 'Beh, proseguiamo!'
 			echo 'Ti ricordo le opzioni: 1.Imposta il mirror Italiano GARR; 2.Installa tutti i software consigliati; 3.Ampliamento funzionalità terminale; 4.Fonts aggiuntivi; 5.Flash Player; 6.Multimedia; 7.Libre Office; 8.Gimp; 9.Natron; 10.Lightworks; 11.RawTherapee; 12.Blue Fish; 13.Inkscape; 14.Blender; 15.OpenSCAD; 16.gDebi; 17.Synaptic; 18.Evince; 19.Bleachbit; 20.Open JDK8; 21.VLC Media Player; 22.Gestore pacchetti Snap; 23.iFuse per supporto Apple iPhone/iPad/iPod; 24.Neofetch; 25.Fonts Google Roboto; 26.Pulizia Distro; 27.Termina'
-	    ;;;;
+	    ;;
 	"Gestore pacchetti Snap")
 	        sudo apt-get install -y snapd
 	        sudo snap install hello
